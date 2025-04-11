908ac8fbcc9867f3687d6aed6e2b7055d40c6ae5
Luiz Miguel <miguelmororo10@gmail.com>
2025-04-11 19:35:22 +0000

    Versão 1.0

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import time
 import pyperclip
 
 # abre o google e seleciona a aba certa
-pyautogui.click(1318,1058, duration=0.5)
+pyautogui.click(1205, 1059, duration=0.5)
 pyautogui.click(629,17, duration=0.5)
 
 # seleciona a primeira célula e copia
@@ -18,33 +18,37 @@ while nome_escola != '-':
     pyautogui.click(316,150, duration=0.5)
     pyautogui.hotkey('ctrl', 'a')
     pyautogui.hotkey('backspace')
-    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.write(f'title:"{nome_escola}"')
+    time.sleep(1)
     pyautogui.hotkey('enter')
 
     # abre o primeiro resultado da pesquisa
-    pyautogui.click(555,373, duration=2, clicks=2)
+    pyautogui.click(555,373, duration=3, clicks=2)
     pyautogui.click(866,19, duration=0.5)
     time.sleep(1)
  
     # ve quantos estagiários tem na escola e salva numa variavel
     pyautogui.hotkey('ctrl', 'f')
     pyautogui.hotkey('ctrl', 'v')
-    pyautogui.moveTo(1706,256, duration=0.5)
+    time.sleep(1)
+    pyautogui.moveTo(1651,255, duration=0.5)
     pyautogui.mouseDown()
-    pyautogui.move(18,0,  duration=1)
+    pyautogui.move(22,0,  duration=1)
     pyautogui.hotkey('ctrl', 'c')
     time.sleep(1)
     pyautogui.mouseUp()
     x = pyperclip.paste()
     estagQTD = int(''.join([char for char in x if char.isdigit()]))
-    pyautogui.click(1857,257, duration=0.5)
+    pyautogui.click(1808, 255, duration=0.5)
 
     # +seleciona e copia todos os estagiarios da escola
-    pyautogui.keyDown('shift')
+    pyautogui.keyDown('shiftleft')
+    pyautogui.keyDown('shiftright')
     pyautogui.hotkey('space')
     for i in range(estagQTD - 1):
         pyautogui.hotkey('down')
-    pyautogui.keyUp('shift')
+    pyautogui.keyUp('shiftleft')
+    pyautogui.keyUp('shiftright')
     pyautogui.hotkey('ctrl', 'c')
 
     # cola as informações da Controle dos estagiarios e cola na pasta da escola
@@ -56,15 +60,16 @@ while nome_escola != '-':
     # ve quantos estagiários tem na PASTA da escola e salva numa variavel
     pyautogui.hotkey('ctrl', 'f')
     pyautogui.write(nome_escola)
-    pyautogui.moveTo(1706,256, duration=0.5)
+    time.sleep(1)
+    pyautogui.moveTo(1652,255, duration=0.5)
     pyautogui.mouseDown()
-    pyautogui.move(18,0,  duration=1)
+    pyautogui.move(22,0,  duration=1)
     pyautogui.hotkey('ctrl', 'c')
     time.sleep(1)
     pyautogui.mouseUp()
     y = pyperclip.paste()
     escolaQTD = int(''.join([char for char in y if char.isdigit()]))
-    pyautogui.click(1857,257, duration=0.5)
+    pyautogui.click(1808, 255, duration=0.5)
 
 
     # apaga a diferença de estagiários
@@ -76,12 +81,14 @@ while nome_escola != '-':
         for i in range(escolaQTD - diff):
             pyautogui.hotkey('down')
 
-        pyautogui.keyDown('shift')
+        pyautogui.keyDown('shiftleft')
+        pyautogui.keyDown('shiftright')
         pyautogui.hotkey('space')
         for i in range(diff):
             pyautogui.hotkey('backspace')
             pyautogui.hotkey('down')
-        pyautogui.keyUp('shift')
+        pyautogui.keyUp('shiftleft')
+        pyautogui.keyUp('shiftright')
         pyautogui.click(728,19, duration=0.5)
         
             
